6bb0a8c7e6d759a159a6a32d8ad093509babbbc0
kevinnls <57634663+kevinnls@users.noreply.github.com>
2021-03-15 12:39:11 +0530

    init installation script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+###################################
+# install script for bak-old.sh   #
+# authored by github.com/kevinnls #
+#   - another Linux user tired of #
+#     type cp file file.bak       #
+###################################
